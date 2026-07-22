f5e0fd42810e605c07c2b771f1979744f2cad03e
Doniyor <doniyor.otamurodov@gmail.com>
2026-07-22 14:23:13 +0500

    feat: tcp socket listener

--- src/main.c
+++ src/main.c
@@ -1,18 +1,28 @@
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int copy_fd(int src, int dst) {
     char buffer[1024];
-    ssize_t n;
 
-    while ((n = read(src, buffer, sizeof(buffer))) > 0) {
+    while (1) {
+        ssize_t n = read(src, buffer, sizeof(buffer));
+
+        if (n == -1) {
+            perror("read");
+            return -1;
+        }
+
+        if (n == 0) return 0;
+
         ssize_t total_written = 0;
 
         while (total_written < n) {
-            ssize_t written = write(dst, buffer + total_written, n - total_written); // written could be 0
+            ssize_t written = write(dst, buffer + total_written, n - total_written);
 
-            if (written == -1) {
+            if (written <= 0) {
                 perror("write");
                 return -1;
             }
@@ -20,39 +30,48 @@ int copy_fd(int src, int dst) {
             total_written += written;
         }
     }
-
-    if (n == -1) {
-        perror("read");
-        return -1;
-    }
-
-    return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: copy <source> <destination>\n");
-        return 1;
+int main(void) {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket");
+        return EXIT_FAILURE;
     }
 
-    int src = open(argv[1], O_RDONLY);
-    if (src == -1) {
-        perror("open");
-        return 1;
+    struct sockaddr_in addr = {.sin_family = AF_INET, .sin_port = htons(8080), .sin_addr.s_addr = htonl(INADDR_ANY)};
+
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("bind");
+        close(server_fd);
+        return EXIT_FAILURE;
     }
 
-    int dst = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    if (dst == -1) {
-        perror("open");
-        close(src);
-        return 1;
+    if (listen(server_fd, 10) == -1) {
+        perror("listen");
+        close(server_fd);
+        return EXIT_FAILURE;
     }
 
-    if (copy_fd(src, dst) == -1) {
-        close(src);
-        close(dst);
-        return -1;
+    printf("Listening on port 8080...\n");
+
+    int client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd == -1) {
+        perror("accept");
+        close(server_fd);
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    printf("Client connected\n");
+
+    if (copy_fd(client_fd, STDOUT_FILENO) == -1) {
+        close(server_fd);
+        close(client_fd);
+        return EXIT_FAILURE;
+    }
+
+    close(server_fd);
+    close(client_fd);
+
+    return EXIT_SUCCESS;
 }
