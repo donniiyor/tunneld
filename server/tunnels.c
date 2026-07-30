c022efb0c45957444c311794852696be96d2cffc
Doniyor <doniyor.otamurodov@gmail.com>
2026-07-30 11:45:25 +0500

    feat: protocol based server

--- server/tunnels.c
+++ server/tunnels.c
@@ -1,7 +1,11 @@
-#include "poll_fds.h"
+#include "connection.h"
+#include "hashtable.h"
+#include "protocol.h"
+#include "vector.h"
 
 #include <netinet/in.h>
 #include <poll.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,6 +14,11 @@
 
 #define PORT 8080
 #define BACKLOG 8
+#define KEY_BUFFER_SIZE 12
+
+typedef struct {
+    uint32_t next_connection_id;
+} server_t;
 
 int main(void) {
     int socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -18,8 +27,8 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    struct sockaddr_in address = {.sin_family = PF_INET, .sin_addr.s_addr = INADDR_ANY, .sin_port = htons(PORT)};
-    if (bind(socket_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+    struct sockaddr_in socket_addres = {.sin_family = PF_INET, .sin_addr.s_addr = INADDR_ANY, .sin_port = htons(PORT)};
+    if (bind(socket_fd, (struct sockaddr *)&socket_addres, sizeof(socket_addres)) == -1) {
         perror("Socket address bind failed");
         close(socket_fd);
         return EXIT_FAILURE;
@@ -33,77 +42,107 @@ int main(void) {
 
     printf("Socket is wating for client connection on port %d...\n", PORT);
 
-    struct poll_fds pfds = {.count = 0};
-    fds_add(&pfds, socket_fd, POLLIN);
+    hashtable_t *connections_by_fd = hashtable_create(sizeof(connection_t) * 10, BACKLOG);
+    hashtable_t *connections_by_id = hashtable_create(sizeof(connection_t) * 10, BACKLOG);
 
-    int client_fd = accept(pfds.fds[0].fd, NULL, NULL);
+    vector_t *poll_fds = vector_create(sizeof(struct pollfd), BACKLOG);
+
+    server_t server = {.next_connection_id = 0};
+
+    struct pollfd socket_pfd = {.fd = socket_fd, .events = POLLIN};
+    vector_push(poll_fds, &socket_pfd);
+
+    int client_fd = accept(socket_fd, NULL, NULL);
     if (client_fd == -1) {
         perror("Failed on client socket accept");
         return EXIT_FAILURE;
     }
 
     printf("Client is connected: %d\n", client_fd);
-    fds_add(&pfds, client_fd, POLLIN);
+
+    struct pollfd client_pfd = {.fd = client_fd, .events = POLLIN};
+    vector_push(poll_fds, &client_pfd);
 
     while (1) {
-        int ready = poll(pfds.fds, pfds.count, -1);
+        int ready = poll(poll_fds->data, poll_fds->size, -1);
         if (ready == -1) {
             perror("poll");
             return EXIT_FAILURE;
         }
 
         // Check for user connections on socket_fd
-        if (pfds.fds[0].revents & POLLIN) {
-            int user_fd = accept(pfds.fds[0].fd, NULL, NULL);
+        if (socket_pfd.revents & POLLIN) {
+            int user_fd = accept(socket_pfd.fd, NULL, NULL);
             if (client_fd == -1) {
                 perror("Failed on user socket accept");
                 continue;
             }
 
-            fds_add(&pfds, user_fd, POLLIN);
             printf("Accepted user socket: %d\n", user_fd);
+
+            struct pollfd *user_pfd = malloc(sizeof(struct pollfd));
+            user_pfd->fd = user_fd;
+            user_pfd->events = POLLIN;
+
+            vector_push(poll_fds, user_pfd);
+
+            connection_t *conn = connection_create(server.next_connection_id++, user_fd);
+
+            hashtable_put(connections_by_fd, &user_fd, sizeof(user_fd), conn, sizeof(connection_t));
+            hashtable_put(connections_by_id, &conn->id, sizeof(conn->id), conn, sizeof(connection_t));
+
+            protocol_send_open(client_fd, conn->id);
+
+            printf("Connection created: %d\n", conn->id);
 
             continue;
         }
 
         // Reading data flow from client socket
-        if (pfds.fds[1].revents & POLLIN) {
-            char buffer[1024];
+        if (client_pfd.revents & POLLIN) {
+            packet_header_t header;
+            protocol_read_header(client_pfd.fd, &header);
 
-            ssize_t n = read(pfds.fds[1].fd, buffer, sizeof(buffer));
-            if (n <= 0) {
-                perror("Reading from client_fd");
+            connection_t *conn;
+            hashtable_get(connections_by_id, &header.connection_id, sizeof(uint32_t), &conn);
 
-                fds_del(&pfds, pfds.fds[1].fd);
-                close(pfds.fds[1].fd);
-
-                return EXIT_FAILURE;
-            }
-
-            // Broadcasting data flow to all user sockets
-            for (size_t i = 2; i < pfds.count; i++) {
-                write(pfds.fds[i].fd, buffer, n);
+            if (header.type == PACKET_DATA) {
+                protocol_read_payload(client_pfd.fd, conn->write_buffer, header.length);
+                write(conn->fd, conn->write_buffer, conn->write_buffer_length);
+                conn->write_buffer_length = 0;
             }
         }
 
-        // Reading from user_fd and writing to client_fd
-        for (size_t i = 2; i < pfds.count; i++) {
-            if (pfds.fds[i].revents & POLLIN) {
-                char buffer[1024];
+        // Reading data from user fds
+        for (size_t i = 2; i < poll_fds->size; i++) {
+            struct pollfd *user_pfd;
+            vector_get(poll_fds, i, &user_pfd);
 
-                ssize_t n = read(pfds.fds[i].fd, buffer, sizeof(buffer));
-                if (n <= 0) {
-                    perror("Reading from user sockets");
+            if (user_pfd->revents & POLLIN) {
+                connection_t *conn;
+                hashtable_get(connections_by_fd, &user_pfd->fd, sizeof(user_pfd->fd), conn);
 
-                    fds_del(&pfds, pfds.fds[i].fd);
-                    close(pfds.fds[i].fd);
+                ssize_t n = read(user_pfd->fd, conn->read_buffer, conn->read_buffer_length);
+                if (n == 0) {
+                    close(user_pfd->fd);
 
-                    printf("Closed user socket connection: %d\n", pfds.fds[i].fd);
+                    vector_erase(poll_fds, i);
+                    hashtable_remove(connections_by_fd, &user_pfd->fd, sizeof(user_pfd->fd));
+                    hashtable_remove(connections_by_id, &conn->id, sizeof(conn->id));
+
+                    protocol_send_close(client_fd, conn->id);
+
+                    printf("Closed user socket connection: %d\n", user_pfd->fd);
+
+                    free(conn);
+                    free(user_pfd);
+
                     continue;
                 }
 
                 // Write data flow to client socket
-                write(pfds.fds[1].fd, buffer, n);
+                protocol_send_data(client_fd, conn->id, conn->read_buffer, conn->read_buffer_length);
+                conn->read_buffer_length = 0;
             }
         }
     }
